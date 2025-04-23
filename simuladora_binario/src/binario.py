f935cc5a0d77d0539c5496eb300e55733b7e4025
marcelorodriog <marcelorodriog@gmail.com>
2025-04-23 18:28:12 -0300

    explicacion de como funciona binario.py

--- simuladora_binario/src/binario.py
+++ simuladora_binario/src/binario.py
@@ -1,14 +1,19 @@
 def decimal_a_binario(n, bits):
-    # devuelve lista de bits de longitud 'bits'
-    resultado = []
-    for i in range(bits):
+                                        # devuelve lista de bits de longitud 'bits'
+    resultado = []                      # crea una lista vacía que almacenara los bits
+    for i in range(bits):               # se itera 'bits' veces
+                                        # se agrega el bit menos significativo (LSB) a la lista resultado
         resultado.append(n % 2)
         n = n // 2
-    # la lista está al revés (LSB primero)
-    return resultado  # ej. [1,0,1] para 5 en 3 bits
+                                        # la lista está al revés (LSB primero)
+    return resultado                    # ej. [1,0,1] para 5 en 3 bits
 
-def binario_a_decimal(bits_list):
-    total = 0
-    for i in range(len(bits_list)):
-        total += bits_list[i] * (2 ** i)
-    return total
+
+def binario_a_decimal(bits_list):           # convierte una lista de bits a decimal
+    total = 0                               # inicializa el total en 0 que almacenara el resultante
+    for i in range(len(bits_list)):         # itera sobre la lista de bits
+        total += bits_list[i] * (2 ** i)    # en cada interación se suma el valor del bit multiplicado por 2 elevado a la posición del bit 
+                                            # el resultado se suma a total
+    return total                            # devuleve el numero decimal calculado
+
+
